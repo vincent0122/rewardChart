d42fb8e7c18759dfacdeb81491171daa18de3be0
vinc <>
2023-02-23 17:59:41 +0900

    pdf.html part is problem

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -5,8 +5,9 @@ import {firestore} from "./Firebase";
 import {jsPDF} from "jspdf";
 import handleImages from "./handleImages";
 
-const NewPost2 = ({images}) => {
+const NewPost = ({images}) => {
   const [faceImages, setFaceImages] = useState([]);
+  const [name, setName] = useState("");
 
   useEffect(() => {
     const loadModels = async () => {
@@ -23,34 +24,104 @@ const NewPost2 = ({images}) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [images]);
 
-  // Save to Firestore
+  const handleSave = () => {
+    const doc = document.getElementById("preview");
+    const pdf = new jsPDF("landscape", "mm", "a4");
 
-  // allFaceImages.forEach(async (faceImage) => {
-  //   try {
-  //     const docRef = await addDoc(collection(firestore, "faces"), {
-  //       image: faceImage.src,
-  //     });
-  //     console.log("Face image added to Firestore with ID: ", docRef.id);
-  //   } catch (e) {
-  //     console.log("Error adding document to Firestore: ", e);
-  //   }
-  // });
+    pdf.html(doc, 10, 10, {
+      width: 190,
+    });
+
+    pdf.save(`${name}'s Reward Chart.pdf`);
+  };
+
+  const previewWidth = "70vw";
+  const faceImageSize = `calc(${previewWidth} / 8 - 10px)`;
 
   return (
-    <div className="container">
-      <div className="left" style={{display: "flex", flexWrap: "wrap"}}>
-        {faceImages.map((faceImage) => (
-          <img
-            key={faceImage.src}
-            crossOrigin="anonymous"
-            src={faceImage.src}
-            alt=""
-            style={{width: "150px", height: "150px", objectFit: "cover"}}
+    <div style={{display: "flex", justifyContent: "space-between"}}>
+      <div
+        id="preview"
+        style={{
+          width: previewWidth,
+          height: "calc(70vw * 1.414)",
+          border: "1px solid black",
+          marginTop: "5px",
+        }}
+      >
+        <div
+          style={{
+            fontSize: "20px",
+            fontWeight: "bold",
+            color: "blue",
+            margin: "10px",
+          }}
+        >
+          {`${name}'s Reward Chart`}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+            flexWrap: "wrap",
+            margin: "10px",
+          }}
+        >
+          {faceImages.map((faceImage, index) => (
+            <div
+              key={`${faceImage.src}-${index}`}
+              style={{
+                width: faceImageSize,
+                height: faceImageSize,
+                borderRadius: "50%",
+                overflow: "hidden",
+                margin: "3px 2px",
+              }}
+            >
+              <img
+                crossOrigin="anonymous"
+                src={faceImage.src}
+                alt=""
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          marginLeft: "5px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            width: "30vw",
+            marginTop: "5px",
+          }}
+        >
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            style={{marginRight: "10px", width: "18vw", height: "5vh"}}
           />
-        ))}
+          <button onClick={handleSave} style={{width: "7vw", height: "5vh"}}>
+            Save
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
-export default NewPost2;
+export default NewPost;
